eee4b2189ad8be6328bf9954c8ab98cbd8b91bcf
Bhoami K Khona <bhoamikhona@gmail.com>
2024-03-06 15:40:00 +0530

    coding challenge 02 and section 10 complete

--- Section 10/coding-challenges.js
+++ Section 10/coding-challenges.js
@@ -158,3 +158,61 @@ poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
  * for us to use these concepts in some different ways to get used to how
  * all of these concepts work together.
  */
+
+/***********************************************************************/
+/************************* CODING CHALLENGE 02 *************************/
+/***********************************************************************/
+
+console.log(
+  '/************************* CODING CHALLENGE 02 *************************/'
+);
+
+/**
+ * This is more of a thinking challenge than a coding challenge.
+ */
+
+// GIVEN
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
+/**
+ * TODO 01:
+ *
+ * Take the IIFE below and at the end of the function, attach an event
+ * listener that changes the color of the selected h1 element ('header')
+ * to blue, each time the body element is clicked. Do not select the h1
+ * element again!
+ */
+
+// done
+
+/**
+ * TODO 02:
+ *
+ * And now explain to yourself (or someone around you) why this worked!
+ * Take all the time you need. Think abut when exactly the callback
+ * function is executed, and what that means for the variables involved
+ * in this example.
+ */
+
+/**
+ * Why did this work? How does the callback function in the event listener
+ * get access to the `header` variable?
+ *
+ * It is because of closure. By the time the callback in the event
+ * listener is executed, the IIFE is long gone i.e. it has executed what
+ * it needed to execute and it's execution context has been removed from
+ * the call stack. Along with it, the `header` variable is gone as well.
+ *
+ * So, even though the environment in which the event listener callback
+ * function was created, is already gone, it is still able to access the
+ * variables that were created in the variable environment, in which the
+ * function was created.
+ */
