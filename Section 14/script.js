85ddc8c60d842b324156a75231dbdbc3b3ac9185
Bhoami K Khona <bhoamikhona@gmail.com>
2024-05-20 11:35:23 +0530

    Coding Challenge 01

--- Section 14/script.js
+++ Section 14/script.js
@@ -195,3 +195,75 @@ console.log(h1.__proto__);
 // remember that functions in JS are objects so they do have prototypes
 // that's why we were able to use methods like bind() on functions before
 console.dir(x => x + 1);
+
+/***********************************************************************/
+/************************* CODING CHALLENGE 01 *************************/
+/***********************************************************************/
+
+console.log(
+  `/************************* CODING CHALLENGE 01 *************************/`
+);
+
+/**
+ * TODO 01:
+ *
+ * Use a constructor function to implement a `Car`. A car has a `make`
+ * and a `speed` property. The `speed` property is the current speed of
+ * the car in km/h.
+ */
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+console.log(Car);
+
+/**
+ * TODO 02:
+ *
+ * Implement an `accelerate` method that will increase the car's speed
+ * by 10, and log the new speed to the console.
+ */
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+console.log(Car.prototype);
+
+/**
+ * TODO 03:
+ *
+ * Implement a `brake` method that will decrease the car's speed by 5, and
+ * log the new speed to the console.
+ */
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+console.log(Car.prototype);
+
+/**
+ * TODO 04:
+ *
+ * Create 2 `Car` objects and experiment with calling `accelerate` and
+ * `brake` mutliple times on each of them.
+ *
+ * Test Data:
+ *
+ * Data Car 01: 'BMW' going at 120 km/h
+ * Data Car 02: 'Mercedes' going at 95 km/h
+ */
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.brake();
+
+mercedes.accelerate();
+mercedes.brake();
