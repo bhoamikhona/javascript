8cd3f9ee7a579895dc36b05f59a237b548454532
Bhoami K Khona <bhoamikhona@gmail.com>
2024-05-25 11:17:55 +0530

    Object.create

--- Section 14/script.js
+++ Section 14/script.js
@@ -435,8 +435,22 @@ const PersonProto = {
   calcAge() {
     console.log(2037 - this.birthYear);
   },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
 };
 
 const steven = Object.create(PersonProto);
 console.log(steven);
 console.log(steven.__proto__);
+
+steven.name = 'Steven';
+steven.birthYear = 2002;
+
+steven.calcAge();
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
